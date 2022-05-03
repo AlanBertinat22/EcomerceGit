0927081af59f1582eaa66ab4f770b6b1bade5e7b
alanbertinat17@gmail.com <alanbertinat17@gmail.com>
2022-05-03 00:11:02 -0300

    Ultimo update del 02/05/2022 pero ya 03 :P

--- src/routes/users.js
+++ src/routes/users.js
@@ -8,4 +8,26 @@ router.get('/users/signup', (req,res) => {
     res.render('users/signup');
 })
 
+router.post('/users/signup', (req,res) => {
+    const {name,email,password,confirmpassword} = req.body;
+    const errors = [];
+    if (name.length <= 0 || email.length <= 0 || password.length <= 0 || confirmpassword.length <= 0  ){
+        errors.push({text: 'Complete todos los campos'});
+    }
+    if (password != confirmpassword) {
+        errors.push({text: 'Las contraseñas no coinciden'});
+    }
+    if (password.length < 4) {
+        errors.push({text: 'La contraseña debe ser mayor a 4 caracteres.'});
+    }
+    if(errors.length > 0 ){
+        res.render('users/signup', {errors, name, email, password, confirmpassword});
+    }
+    else{
+        res.send('ok');
+    }
+    console.log(errors);
+    console.log(req.body);
+});
+
 module.exports = router;
